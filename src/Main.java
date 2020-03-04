5b74148661d2540de994cc8d71e0ffd551930c11
SersanAslan <s185021@student.dtu.dk>
2020-03-04 10:58:03 +0100

    motor virker !!!

--- src/Main.java
+++ src/Main.java
@@ -1,28 +1,55 @@
+import lejos.hardware.Brick;
 import lejos.hardware.BrickFinder;
-import lejos.remote.ev3.RMIRegulatedMotor;
+import lejos.hardware.BrickInfo;
+import lejos.hardware.Sound;
+import lejos.hardware.motor.UnregulatedMotor;
+import lejos.hardware.port.MotorPort;
 import lejos.remote.ev3.RemoteEV3;
+import lejos.utility.Delay;
 
+import java.net.MalformedURLException;
+import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
 
 public class Main {
 
-    public static void main(String[] args) throws RemoteException {
+    public static void main(String[] args) throws RemoteException, NotBoundException, MalformedURLException {
+
+        RemoteEV3 ev3;
+        BrickInfo[] bricks = BrickFinder.discover();
+
+        for (BrickInfo info : bricks) {
+            System.out.println(info.getIPAddress());
+            Brick brick = new RemoteEV3(info.getIPAddress());
+            brick.getAudio().systemSound(0);
+
+        }
+
+        ev3 = new RemoteEV3(bricks[0].getIPAddress());
+        ev3.setDefault();
+
+        // create two motor objects to control the motors.
+        UnregulatedMotor motorA = new UnregulatedMotor(MotorPort.B);
+        UnregulatedMotor motorB = new UnregulatedMotor(MotorPort.C);
+
+        // set motors to 50% power.
+        motorA.setPower(50);
+        motorB.setPower(50);
+
+        // wait 2 seconds.
+        Delay.msDelay(2000);
+
+        // stop motors with brakes on.
+        motorA.stop();
+        motorB.stop();
+
+        // free up motor resources.
+        motorA.close();
+        motorB.close();
+
+        Sound.beepSequence(); // we are done.
 
 
-        // Detecting EV3 Brick
-        RemoteEV3 ev3 = (RemoteEV3) BrickFinder.getDefault();
-
-        // Creating objects for motor and IR sensor
-        RMIRegulatedMotor leftMotor = ev3.createRegulatedMotor("B", 'L');
-        RMIRegulatedMotor rightMotor = ev3.createRegulatedMotor("C", 'L');
-
-        leftMotor.setSpeed(-50);
-        leftMotor.forward();
-        rightMotor.setSpeed(-50);
-        rightMotor.forward();
-
-        leftMotor.stop(true);
-        rightMotor.stop(true);
 
     }
 }
